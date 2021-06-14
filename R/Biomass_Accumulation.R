dd8765e1f2974b6eb2f36ed9baa2f614c2972261
SJW <sjw@Sarahs-Vacuum-Cleaner.local>
2021-06-14 17:50:09 -0400

    cleaned up directory

---
+++ R/Biomass_Accumulation.R
@@ -0,0 +1,39 @@
+#Title: GOM Rpath Biomass Accumulation
+
+# Purpose: This script generates estimates of biomass accumulation for 
+#       the single-species functional groups used in the GOM-Rpath model.
+#       Data are sourced from the NEFSC bottom trawl. Model is built
+#       to be analogous to Georges Bank Rpath model
+#       https://github.com/NOAA-EDAB/GBRpath
+
+# DataFile:'NEFSC_BTS_2021_all_seasons.RData'
+
+# Author: S. Weisberg
+# Contact details: sarah.j.weisberg@stonybrook.edu
+
+xfun::session_info()
+
+# Mon Jun 14 17:10:51 2021 ------------------------------
+#Run survey biomass estimate code
+source('R/survey_biomass_estimates.R')
+
+# Pull out Rpath groups from survey data
+survey.groups <- as.data.table(unique(swept$RPATH))
+colnames(survey.groups)<-"RPATH"
+survey.groups<-na.omit(survey.groups)
+
+#Remove units
+swept$Biomass<-as.numeric(swept$Biomass)
+
+#Fit linear model to biomass trends all surveyed RPATH groups, extract slope
+ba<-c()
+p<-c()
+for (i in 1:length(survey.groups$RPATH)){
+  lm<-lm(Biomass~YEAR, data = subset(swept, RPATH == survey.groups$RPATH[i]))
+  spF <- as.numeric(summary(lm)$fstatistic)
+  p[i] <- pf(spF[1], spF[2], spF[3], lower = F)
+  ba[i] <-as.numeric(lm[[1]][2])
+}
+
+biomass.accum<-cbind(survey.groups,ba,p)
+biomass.accum<-subset(biomass.accum, p<=0.05 & abs(ba) >= 0.005)
