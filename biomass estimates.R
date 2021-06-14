3169877dba4208bfc44e3a9f5bf1e3c355d99b89
SJW <sjw@Sarahs-Vacuum-Cleaner.local>
2021-06-14 16:05:14 -0400

    Use updated survdat functions for biomass

--- biomass estimates.R
+++ biomass estimates.R
@@ -1,108 +1,82 @@
-#Calculate biomass estimates for RPath groups
-#Using Sean's stratified mean biomass & swept area biomass functions for surveyed groups
-#Using EMAX values for others
+#Title: GOM Rpath Biomass estimates
 
-library(data.table); library(rgdal); library(Survdat); library(here)
+# Purpose: This script generates biomass estimates for 
+#       the single-species functional groups usedin the GOM-Rpath model.
+#       Data are sourced from the NEFSC bottom trawl. Model is parameterized
+#       for 1980-85 using fall survey data only. Model is built
+#       to be analogous to Georges Bank Rpath model
+#       https://github.com/NOAA-EDAB/GBRpath
 
-here()
-#User define
+# DataFile:'NEFSC_BTS_2021_all_seasons.RData'
 
-'%notin%' <-Negate('%in%')
+# Author: S. Weisberg
+# Contact details: sarah.j.weisberg@stonybrook.edu
 
-#Grab survdat.r
-#load(file.path(data.dir, 'Survdat.RData'))
-#Load species list
-load("Survdat.RData")
+xfun::session_info()
+# Mon Jun 14 14:55:48 2021 ------------------------------
 
-#Load species list
-load("Species_codes.RData")
+#Load needed packages
+#Load required packages
+library(survdat); library(ggplot2); library(rgdal); library(here);library(readr)
 
-#Grab strata
-#strata <- readOGR(gis.dir, 'strata')
+#Load survey data
+load('data/NEFSC_BTS_2021_all_seasons.RData')
+
+#Load species codes
+#Use these codes to translate survey species codes ('SVSPP') to RPATH species codes ('spp')
+load("speciescodesandstrata/Species_codes.Rdata")
+
+#Load strata
 strata<- readOGR('speciescodesandstrata','strata')
 
-#Generate area table
-strat.area<- getarea(strata, 'STRATA')
-setnames(strat.area, 'STRATA', 'STRATUM')
+#Calculate total GOM area
+area<-sf::st_read(dsn=system.file("extdata","strata.shp",package="survdat"))
+area<-get_area(areaPolygon = area, areaDescription="STRATA")
+GOM.area<-subset(area, area$STRATUM %in% c(1220, 1240, 1260:1290, 1360:1400, 3560:3830))
+GOM.area<-sum(GOM.area$Area)
 
-#Input species list
-GOM.groups <-as.data.table(c('Phytoplankton', 'Bacteria', 'Microzooplankton', 'GelZooplankton','LgCopepods', 'SmCopepods', 'Micronekton', 'Illex',' Loligo', 'OtherCephalopods','Macrobenthos','AmLobster','OceanPout','SmPelagics','SmFlatfishes','NShrimp','RiverHerring','AtlScallop','OtherPelagics','SummerFlounder','AtlHerring','Mesopelagics','SouthernDemersals','YTFlounder','Cod','Haddock','AmPlaice','AtlMackerel','Cusk','OtherDemersals','HMS','OtherSkates','RedHake','Barndoor Skate','Fourspot','SmoothDogfish','Pollock','Goosefish','SilverHake','WhiteHake','SpinyDogfish','Redfish','LittleSkate','SeaBirds','Windowpane','WinterFlounder','WitchFlounder','BlackSeaBass','Sharks','WinterSkate','Pinnipeds','BaleenWhales','Odontocetes'))
-setnames(GOM.groups,'V1','RPATH')
+#Calculate mean stratified biomass
+#GOM strata
+#Fall season only
+stratmean<-calc_stratified_mean(surveyData=survey$survdat, areaPolygon = 'NEFSC strata', areaDescription = 'STRATA', filterByArea = c(1220, 1240, 1260:1290, 1360:1400, 3560:3830), filterBySeason= "FALL", groupDescription = "SVSPP", filterByGroup = "all", mergesexFlag = T, tidy = F, returnPrepData = F)
 
-#Subset by season/ strata set - fall for GOM (inc. inshore)
-fall.GOM <- survdat[SEASON == 'FALL' & STRATUM %in% c(1220, 1240, 1260:1290, 1360:1400, 3560:3830), ]
+#Merge stratified means with RPATH names
+stratmean<-merge(stratmean,spp[,list(SVSPP,RPATH,SCINAME,Fall.q)], by = 'SVSPP')
 
-#Run stratification prep
-setnames(fall.GOM, 'EST_YEAR', 'YEAR')
-GOM.prep <- stratprep(fall.GOM, strat.area, strat.col = 'STRATUM', area.col = 'Area')
+#Calculate swept area biomass
+swept<-calc_swept_area(surveyData=survey$survdat, areaPolygon = 'NEFSC strata', areaDescription = 'STRATA', filterByArea = c(1220, 1240, 1260:1290, 1360:1400, 3560:3830), filterBySeason= "FALL", groupDescription = "SVSPP", filterByGroup = "all", mergesexFlag = T,tidy = F, q = NULL, a = 0.0384)
+
+#Merge swept area biomass with RPATH names
+swept<-merge(swept,spp[,list(SVSPP,RPATH,SCINAME,Fall.q)], by = 'SVSPP')
 
 #Merge with RPATH names
-spp <- spp[!duplicated(spp$SVSPP),]
-#GOM.prep<-merge(GOM.prep, spp[,list(SVSPP,COMNAME,RPATH,SCINAME)], by = 'SVSPP')
+#RPATH names are the same as in Georges Bank model
+#Aggregate groups below 0.05 t/km^2 threshold
+spp <- spp[!duplicated(spp$SVSPP),] #SVSPPs are NEFSC species-specific codes
+spp <- spp[RPATH == 'RedCrab', RPATH := 'Macrobenthos']
+#spp <- spp[RPATH == 'AtlScallop', RPATH := 'Megabenthos'] #Need to come back to this!
+spp <- spp[RPATH == 'Clams', RPATH := 'Megabenthos']
+spp <- spp[RPATH == 'Scup', RPATH := 'OtherDemersals']
+spp <- spp[RPATH == 'OffHake', RPATH := 'OtherDemersals']
+spp <- spp[RPATH == 'Bluefish', RPATH := 'OtherPelagics']
+spp <- spp[RPATH == 'AmShad', RPATH := 'SmPelagics']
+spp <- spp[RPATH == 'SmallPelagics', RPATH := 'SmPelagics']
+spp <- spp[RPATH == 'AtlCroaker', RPATH := 'SouthernDemersals']
+spp <- spp[RPATH == 'LargePelagics', RPATH := 'OtherPelagics']
+spp <- spp[RPATH == 'OtherFlatfish', RPATH := 'OtherDemersals']
+spp <- spp[RPATH == 'StripedBass', RPATH := 'OtherPelagics']
+spp <- spp[RPATH == 'Sturgeon', RPATH := 'OtherDemersals']
+spp <- spp[RPATH == 'Weakfish', RPATH := 'OtherDemersals']
 
-#Calculate stratified means 
-mean.biomass <- stratmean(GOM.prep, group.col = 'SVSPP', 
-                      strat.col = 'STRATUM')
-#merge with RPATH names
-mean.biomass<-merge(mean.biomass,spp[,list(SVSPP,RPATH,SCINAME,Fall.q)], by = 'SVSPP')
+#Cusk not included in Georges Bank model -- need to create RPATH name
+spp <- spp[SCINAME == 'BROSME BROSME', RPATH := 'Cusk']
 
-#Calculate total biomass/abundance estimates
-total.biomass <- sweptarea(GOM.prep, mean.biomass, strat.col = 'STRATUM', area.col = 'Area')
+#Calcuate biomass / area in mt
+swept <- swept[, biomass.area   := (tot.biomass*.001)/(Fall.q*GOM.area)]
 
-#merge with RPATH names
-total.biomass<-merge(total.biomass,spp[,list(SVSPP,RPATH,SCINAME,Fall.q)], by = 'SVSPP')
-
-#total area
-GOM.strat.area <- strat.area[STRATUM %in% c(1220, 1240, 1260:1290, 1360:1400, 3560:3830), sum(Area)]
-
-#biomass / area in mt
-total.biomass <- total.biomass[, biomass.t_area :=(tot.biomass*.001)/(Fall.q*GOM.strat.area)]
-
-#average over 1980s
-GOM.biomass.80s <- total.biomass[YEAR %in% 1980:1985, mean(biomass.t_area), by = RPATH]
-setnames(GOM.biomass.80s,'V1','Biomass')
-
-#Output results to csv
-write.csv(GOM.biomass.80s, 'Mean Biomass_GoM_1980-85.csv')
-
-##add EMAX groups
-GOM.EMAX<-as.data.table(read.csv('GOM_EMAX_params.csv'))
-
-##Merge group list with survey biomass estimates
-GOM.groups<-merge(GOM.biomass.80s, GOM.groups, by = 'RPATH', all.y=TRUE)
-setnames(GOM.groups,'V1','Biomass', skip_absent = TRUE)
-
-#Filter EMAX model for group, biomass
-setnames(GOM.EMAX,'model.Group','RPATH')
-setnames(GOM.EMAX,'model.Biomass','Biomass')
-GOM.EMAX<-GOM.EMAX[, c('RPATH','Biomass')]
-
-#Change EMAX names to match RPATH names
-GOM.EMAX<-GOM.EMAX[RPATH == 'Small copepods', RPATH := 'SmCopepods',]
-GOM.EMAX<-GOM.EMAX[RPATH == 'Large Copepods', RPATH := 'LgCopepods',]
-GOM.EMAX<-GOM.EMAX[RPATH == 'Sea Birds', RPATH := 'SeaBirds',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Macro', RPATH:='Macrobenthos',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Baleen', RPATH := 'BaleenWhales',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Gel',RPATH := 'GelZooplankton',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Phyto', RPATH :='Phytoplankton',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Megabenthos',RPATH := 'Mega',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Pelagics',RPATH := 'Pelagics',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Demersals',RPATH := 'Demersals',]
-GOM.EMAX<-GOM.EMAX[RPATH %like% 'Sharks', RPATH :='Sharks']
-
-#Aggregate EMAX macrobenthos groups into one 
-BenthosBiomass<-GOM.EMAX[RPATH == 'Macrobenthos', sum(Biomass),]
-GOM.EMAX<-GOM.EMAX[RPATH == 'Macrobenthos', Biomass:=BenthosBiomass,]
-
-#Remove groups not included in Rpath model
-GOM.EMAX<- GOM.EMAX[RPATH %notin% c('Larval-juv fish- all','Shrimp et al.','Mega','Pelagics','Demersals','Discard','Detritus-POC','Fishery'),]
-GOM.EMAX<-unique(GOM.EMAX)
-
-#Merge survey and EMAX biomass estimates
-GOM.biomass.80s<-merge(GOM.groups,GOM.EMAX,by='RPATH', all=TRUE)
-GOM.biomass.80s<-GOM.biomass.80s[is.na(Biomass.y) , Biomass.y := Biomass.x]
-GOM.biomass.80s<-GOM.biomass.80s[,Biomass.x :=NULL]
-setnames(GOM.biomass.80s,"Biomass.y","Biomass")
-
-#Save as .csv
-write.csv(GOM.biomass, 'GOM_Parameters_V1.csv')
+#average over 1980-85
+setkey(swept,RPATH,YEAR)
+swept <- swept[, sum(biomass.area), by = key(swept)]
+setnames(swept, 'V1','Biomass')
+biomass.80s<-swept[YEAR %in% 1980:1985, mean(Biomass), by=RPATH]
+setnames(biomass.80s, 'V1','Biomass')
