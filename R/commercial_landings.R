b201adccf2d58f17245eb1ba0cf48d519235b3fa
Sean Lucey <Sean.Lucey@NOAA.gov>
2021-06-16 15:02:56 -0400

    Split landings in stat area 521, 522, 561

--- R/commercial_landings.R
+++ R/commercial_landings.R
@@ -11,15 +11,25 @@ path.yrs <- 1981:1985
 comland <- comlandr::get_comland_data(channel, filterByYear = path.yrs, useForeign = F)
 
 #Assigned area to GOM
-#Will be using NAFO Stat Areas - 
+#Will be using NAFO Stat Areas - 500, 510, 512, 513, 514, 515, 521, 522, 561
+#Several stat areas are completely within the GOM EPU
+full.areas <- c(500, 510, 512, 513, 514, 515)
+
+landings <- comland$comland[AREA %in% full.areas, ]
+
 #Areas 521, 522, and 561 straddle Georges Bank.  The proprotion of catch inside
 #and outside was calulated from the MS Keyrun project for Georges Bank.  We will use
 #the proportion outside of Georges Bank for those Stat Areas.  The rest will be 
 #100% GOM
-
 proportions <- readRDS(here('data/All_Species_Proportions.rds')) 
 proportions <- proportions[AREA %in% c(521, 522, 561) & InOut == 'out', ]  
 
+gom.split.land <- assign_area(comland, proportions, areaDescription = 'InOut', 
+                              propDescription = 'MeanProp')
 
-comland <- assign_area(comland, userAreas, areaDescription = 'EPU', 
-                       propDescription = 'Prop')
+#Merge split landings with other landings
+landings <- rbindlist(list(landings, gom.split.land$comlands))
+
+#Assign gears to fleets
+
+
