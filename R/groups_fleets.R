ab8a6c6b574d9b94dbd079824c9b24943aa6e14c
SJW <sjw@Sarahs-Vacuum-Cleaner.local>
2021-07-22 16:56:05 -0400

    merging from Sean's changes

--- R/groups_fleets.R
+++ R/groups_fleets.R
@@ -14,7 +14,8 @@
 #Load needed packages
 library(here);library(data.table)
 
-#Load landings data
+#Load landings dataload
+load(here("data/mean_landings_gom_80_85.RData"))
 
 #Change "HMS" to "HMS Fleet" to avoid confusion
 mean.land[FLEET =="HMS",FLEET:="HMS Fleet"]
