5a7fb32ee66f6a819f6ff64b131d652a6be494c1
SJW <sjw@Sarahs-Vacuum-Cleaner.local>
2021-09-23 11:29:11 -0400

    Script to get Rpath model from Ecosense/Rsim output

---
+++ Rsim_Rpath_conversion.R
@@ -0,0 +1,70 @@
+#Script to convert Ecosense output (i.e. Rsim scenario) to Rpath object
+
+#Author: Sarah J. Weisberg
+
+# Tue Sep 21 09:14:35 2021 ------------------------------
+
+#Run GOM_sense
+source(here('R/GOM_sense.R'))
+
+#Copy initial Rpath parameters
+#Alternative scenarios will be the same except for Biomass, PB, QB, Diet, Landings
+alt<-copy(REco.params)
+alt.diet<-copy(REco.params$diet)
+
+#Count number of each group type
+ngroups <- nrow(alt$model)
+nliving <- nrow(alt$model[Type <  2, ])
+ndead   <- nrow(alt$model[Type == 2, ])
+ngear   <- nrow(alt$model[Type == 3, ])
+
+#Pull out individual Ecosense scenario
+#Eventually will need to loop this over all successes
+REco.sense1<-REco.sense[[1]]
+
+#Assign biomass
+alt$model[,Biomass:=REco.sense1$B_BaseRef[-1]]
+
+#Assign PB
+alt$model[,PB:=REco.sense1$PBopt[-1]]
+
+#Assign QB
+alt.QB<-REco.sense1$FtimeQBOpt[-1]
+alt.QB[1]<-0 #How to make this generalizable to models with other PP groups?
+alt$model[,QB:=alt.QB]
+
+#Assign diet
+#Remove first two columns which represent 'outside' and 'PP'
+PreyFrom<-REco.sense1$PreyFrom[-c(1,2)]
+PreyTo<-REco.sense1$PreyTo[-c(1,2)]
+predpreyQ<-REco.sense1$QQ[-c(1,2)]
+
+#Fill consumption matrix
+QQ<-matrix(nrow = (nliving + ndead + 1),ncol=nliving)
+
+for (i in 1:length(PreyFrom)){
+  prey<-PreyFrom[i]
+  pred<-PreyTo[i]
+  QQ[prey,pred]<-predpreyQ[i]
+}
+
+#Convert consumption matrix to diet matrix
+#Divide consumption by (QB*B)
+diet<-matrix(nrow = (nliving + ndead + 1),ncol=nliving)
+ 
+for (i in 1:nliving){
+  diet[,i]<-test.QQ[,i]/(alt.QB[i]*alt.biomass[i])
+}
+
+#diet[is.na(diet)]<-0
+
+#Convert to data table and fill in Rpath format
+alt.diet[,2:(nliving+1)]<-as.data.table(diet)
+alt$diet<-alt.diet
+
+#Fill landings?
+
+AltEco1 <- rpath(alt, eco.name = 'GOM Ecosystem Alt1')
+AltEco1
+
+check.rpath.params(alt)
