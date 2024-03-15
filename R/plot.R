dcbfeb91215d306cd6e1e7b7e74dd5fcdf8329a5
SarahJWeisberg <sarah.j.weisberg@stonybrook.edu>
2024-03-15 15:38:30 -0400

    adding network plot code

--- R/plot.R
+++ R/plot.R
@@ -1,24 +1,28 @@
 #code to visualize GOM food web
 #uses network model
 
-# Tue Mar  5 13:29:10 2024 ------------------------------
+# Fri Mar 15 15:37:46 2024 ------------------------------
 
 #load required packages
 library(igraph)
 library(ggplot2)
 library(ggnetwork)
+library(viridis)
+library(devtools)
+#need to install an older version of gdata to get enaR to wrok
+install.packages("gdata", repos = "https://packagemanager.posit.co/cran/2023-05-06") 
+install_github('SEELab/enaR', force = T)
 
 #first run enaR_sense.R to get orig.network
 
 #pull network structure
-source(here("enaStructure.R"))
 A<-enaStructure(orig.network)$A
 g<-igraph::graph_from_adjacency_matrix(A)
 
 #get x and y coordinates
 #derived from the plotfw function (https://rfrelat.github.io/BalticFoodWeb.html)
 nylevel<-7 #determines number of levels along y-axis
-n <- vcount(g) #number of vertices
+n <- igraph::vcount(g) #number of vertices
 tl<-GOM$TL[1:(nliving+ndead)] #pull trophic level calculations from rpath
 bks <- c(0.9, seq(1.9, max(tl), length.out = nylevel))
 ynod <- cut(tl, breaks = bks, include.lowest = TRUE, 
@@ -38,9 +42,11 @@ for (i in 1:nylevel){
 
 coo <- cbind(xnod,tl) #these inform x and y coordinates
 
-#scootch Bacteria to the left to get more space in the bottom of the web
-#original x and y
-coo[2,1]<-(-14)
+#make manual adjustments if necessary
+#eg., scootch Bacteria to the left to get more space in the bottom of the web
+coo[2,1]<-(-10)
+coo[31,1]<-7
+coo[48,1]<-16
 
 #use ggnetwork to create a network geometry
 #storage and flows from original model
@@ -50,30 +56,29 @@ TL<-as.data.frame(cbind(GOM$TL,groups)) %>% rename(vertex.names=groups,TL=V1)
 TL<-TL %>% mutate(TL = as.numeric(TL)) %>% mutate(TL = round(TL,2))
 n<-left_join(n,TL,by="vertex.names")
 
-#set minimum size
-n<-n %>% mutate(storage_adjust = ifelse(storage < 0.1,storage*16,storage))
 #adjust Detritus storage so it doesn't swamp everything
-n <- n %>% mutate(storage_adjust = ifelse(storage >50,sqrt(storage),storage_adjust))
+det<-max(GOM$Biomass)
+n <- n %>% mutate(storage_adjust = ifelse(storage == det,100,storage))
 
-#scootch Bacteria to the left to get more space in the bottom of the web
-#original x and y
-#x_bac<-n %>% filter(vertex.names == "Bacteria") %>% select(x) %>% distinct()
-#y_bac<-n %>% filter(vertex.names == "Bacteria") %>% select(y) %>% distinct()
-#n <- n %>% mutate(x = ifelse(vertex.names =="Bacteria", 0.1,x)) %>% 
- # mutate(xend = ifelse(xend == x_bac$x & yend == y_bac$y, 0.1,xend))
-
+#plot!
 GOM_web<-ggplot(n,aes(x, y, xend = xend, yend = yend)) +
   geom_edges(arrow = arrow(length = unit(7, "pt"), type = "open"),
-             curvature = 0.175,position="jitter",
+             curvature = 0.15,position="jitter",
              aes(color=TL,linewidth=flow)) +
-  scale_color_viridis(option = "turbo")+
-  scale_linewidth(range = c(0.2,10))+
-  geom_nodelabel(aes(label=vertex.names,size=(sqrt(storage_adjust))),show.legend = F) +
-  guides(linewidth=F)+
-  theme_blank()
+  scale_color_gradientn(colors = turbo(6))+
+  scale_linewidth(range = c(0.15,9))+
+  geom_nodelabel(aes(label=vertex.names,size=((storage_adjust))),show.legend = F) +
+  scale_size(range=c(2.5,10))+
+  guides(linewidth="none")+
+  annotate("text",x=0.025,y=0.975,label="GOM",size=9)+
+  theme_blank(legend.position="none")+
+  theme_blank(legend.position=c(0.8,0.2))+
+  theme(panel.background = element_rect(fill="#EEEEEEFF"),
+        plot.background = element_rect(fill="#EEEEEEFF"),
+        legend.background=element_rect(fill="#EEEEEEFF"))
 
 GOM_web
 
-ggsave(filename = "outputs/webplot_GOM.pdf", width = 40, height=25,units = c("cm"), plot=GOM_web,dpi=300)
 
-ggsave(filename = "outputs/webplot_GOM.pdf",plot=GOM_web,dpi=300)
+ggsave(filename = "outputs/webplot_GOM.png",width = 30, height=15,units = c("cm"),plot=GOM_web,dpi=300)
+
